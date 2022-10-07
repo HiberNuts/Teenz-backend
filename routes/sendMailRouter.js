66703faa47f56fe41d46cb8fbbfdf6c0b0c4db6f
raghav <raghavjindal0212@gmail.com>
2022-10-07 09:03:59 +0530

    env changes

--- routes/sendMailRouter.js
+++ routes/sendMailRouter.js
@@ -11,7 +11,7 @@ let transporter = nodemailer.createTransport({
   auth: {
     type: "OAuth2",
     user: process.env.MAIL_USERNAME,
-    pass: process.env.MAIL_PASSWORD,
+
     clientId: process.env.OAUTH_CLIENTID,
     clientSecret: process.env.OAUTH_CLIENT_SECRET,
     refreshToken: process.env.OAUTH_REFRESH_TOKEN,
