0c665dd1f181e8b2872740ed2c3f426703da6649
raghav <raghavjindal0212@gmail.com>
2022-12-03 00:07:37 +0530

    changes

--- routes/feedbackDataRoute.js
+++ routes/feedbackDataRoute.js
@@ -27,8 +27,9 @@ feedbackRouter.route("/feedback").post(feedback.postFeedbackData);
 feedbackRouter.route("/feedback").get(feedback.getFeedbackData);
 
 feedbackRouter.route("/feedback/photo/:id").post(upload("feedback").single("file"), async (req, res, next) => {
-  // const url = `${req.protocol}://${req.get("host")}`;
-  const url = "http://3.88.157.171:8080";
+  const url = `${req.protocol}://${req.get("host")}`;
+  // const url = "https://tinarosario.com/route";
+  // const url = "http://localhost/route";
   const { file } = req;
   const feedbackID = req.params.id;
   try {
